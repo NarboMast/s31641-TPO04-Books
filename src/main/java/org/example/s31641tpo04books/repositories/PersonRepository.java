f312e940aaf75ca61fbe223fe7e059f61ea4ec65
NarboMast <narboto.az2005@gmail.com>
2025-03-30 21:30:44 +0200

    OK

--- src/main/java/org/example/s31641tpo04books/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.s31641tpo04books.repositories;
-
-import org.example.s31641tpo04books.entities.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
--- src/main/java/org/example/s31641tpo04books/repositories/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.s31641tpo04books.repositories;
-
-import org.example.s31641tpo04books.entities.Publisher;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
